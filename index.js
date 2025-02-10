9d1ad9c73a808f14a1631f163734778855171480
muhammedAslam <aslamap707@gmail.com>
2025-02-10 18:40:04 +0530

    UMS

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path')
+
+const mongoUrl = "mongodb://localhost:27017/user_management_system"
+async function connect() {
+    try {
+        await mongoose.connect(mongoUrl);
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+    }
+}
+
+connect();
+const app = express();
+const port =4000;
+
+app.use(express.static('./public'))
+
+const adminRoute = require('./routes/adminRoutes')
+app.use('/admin', adminRoute)
+
+const userRoute = require('./routes/userRoutes')
+app.use('/',userRoute )
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
